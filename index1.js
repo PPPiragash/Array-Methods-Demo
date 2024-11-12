8fd95b237469c5142a9f1d4b03dd7e3d31df97bc
PPPiragash <pppiragash2004@gmail.com>
2024-11-12 09:17:17 +0530

    Added array methods demo page with functionality to add, remove, sort, and reverse array elements.

---
+++ index1.js
@@ -0,0 +1,32 @@
+// let array = ["ab","ef","cd"];
+// // document.write(array.toString()); 
+
+
+// // document.write(array.at(2));
+
+// // document.write(array.join("-"));
+
+// // array.pop();
+// // document.write(array);
+
+// array.push("gh","ij","mn");
+// // document.write(array);
+
+// // document.write(array.splice(0,3));
+
+// let array2=[1,2,3,4];
+// // document.write(array.concat(array2));
+
+// // document.write(array.slice(0,4));
+
+// // array.sort();
+// // document.write(array);
+
+// // array.reverse();
+// // document.write(array);
+
+// // document.write(array2.indexOf(2));
+// // document.write(array2.indexOf(10));
+
+// document.write(array2.includes(3));
+// document.write(array2.includes(7));
